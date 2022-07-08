5128cfba5c9e3f5b3583adba2a19c2b3eab7b6c9
christopherw1 <chriswang3718@gmail.com>
2022-07-08 09:44:13 -0500

    button layout adjustment

--- src/App.tsx
+++ src/App.tsx
@@ -4,32 +4,6 @@ import PokemonCard from './PokemonCard/PokemonCard';
 import PokemonSearch from './PokemonSearch/PokemonSearch';
 //import ReactDOM from 'react-dom/client';
 
-function getPokeListRange(region: number) {
-  let rangeStart = 1;
-  let rangeEnd = 151;
-  if (region === 1) {
-    rangeStart = 1;
-    rangeEnd = 151;
-  } else if (region === 2) {
-    rangeStart = 152;
-    rangeEnd = 251;
-  } else if (region === 3) {
-    rangeStart = 252;
-    rangeEnd = 386;
-  } else if (region === 4) {
-    rangeStart = 387;
-    rangeEnd = 493;
-  } else {
-    rangeStart = 494;
-    rangeEnd = 649;
-  }
-  let pokeList = [];
-  for(let i = rangeStart; i <= rangeEnd; i++) {
-    pokeList.push(<PokemonCard pokeID={i.toString()}></PokemonCard>)
-  }
-  return pokeList;
-}
-
 function App() {
   const [ region, setRegion ] = useState(1);
 
@@ -58,11 +32,15 @@ function App() {
         <PokemonSearch></PokemonSearch><br></br>
       </header>
       <body className="App-body inline-grid">
-        <button onClick={() => setRegion((c) => 1)}>Kanto</button>
-        <button onClick={() => setRegion((c) => 2)}>Johto</button>
-        <button onClick={() => setRegion((c) => 3)}>Hoenn</button>
-        <button onClick={() => setRegion((c) => 4)}>Sinnoh</button>
-        <button onClick={() => setRegion((c) => 5)}>Unova</button>
+        <p>Or pick a region</p>
+        <div className="inline-grid grid-cols-6 gap-4">
+          <button onClick={() => setRegion((c) => 1)}>Kanto</button>
+          <button onClick={() => setRegion((c) => 2)}>Johto</button>
+          <button onClick={() => setRegion((c) => 3)}>Hoenn</button>
+          <button onClick={() => setRegion((c) => 4)}>Sinnoh</button>
+          <button onClick={() => setRegion((c) => 5)}>Unova</button>
+          <button onClick={() => setRegion((c) => 6)}>Kalos</button>
+        </div>
         <br></br>
         {/* {Array.from({ length: 150 }).map((_, index) => {
           return <PokemonCard pokeID={index + 1 + ""}></PokemonCard>
@@ -73,6 +51,37 @@ function App() {
       </body>
     </div>
   );
+}
+
+
+
+function getPokeListRange(region: number) {
+  let rangeStart = 1;
+  let rangeEnd = 151;
+  if (region === 1) {
+    rangeStart = 1;
+    rangeEnd = 151;
+  } else if (region === 2) {
+    rangeStart = 152;
+    rangeEnd = 251;
+  } else if (region === 3) {
+    rangeStart = 252;
+    rangeEnd = 386;
+  } else if (region === 4) {
+    rangeStart = 387;
+    rangeEnd = 493;
+  } else if (region === 5) {
+    rangeStart = 494;
+    rangeEnd = 649;
+  } else {
+    rangeStart = 650;
+    rangeEnd = 721;
+  }
+  let pokeList = [];
+  for(let i = rangeStart; i <= rangeEnd; i++) {
+    pokeList.push(<PokemonCard pokeID={i.toString()}></PokemonCard>)
+  }
+  return pokeList;
 }
 
 export default App;
